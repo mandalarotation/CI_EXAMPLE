bbe2da97bd66cf640ad8996c362948dfe4323260
Jean Carlo Jimenez Giraldo <jecjimenezgi@unal.edu.co>
2021-09-01 19:45:48 -0500

    Creando prueba unitaria

---
+++ __tests__/challenge1.test.js
@@ -0,0 +1,12 @@
+const { suma } = require("../index");
+
+test("Debería regresar 8 al pasar como argumentos 6 y 2", () => {
+  let a = 6;
+  let b = 2;
+
+  let result = suma(a, b);
+
+  expect(result).toBe(8);
+});
+
+
